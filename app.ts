2fe0c229f2c29469cc2d376ac85c6c4ac0c7610a
JessycaTaynara <jessycatay16@gmail.com>
2023-12-25 20:11:00 -0300

    arquivo de instância do express

---
+++ app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import routes from "./src/routes/routes"
+
+const app = express()
+const porta = 3000
+
+app.use(express.json)
+app.use(routes)
+app.listen(porta, ()=>{
+    console.log(`API rodando na porta ${porta}`)
+})
