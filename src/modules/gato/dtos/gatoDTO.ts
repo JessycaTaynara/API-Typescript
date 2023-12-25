316e652f13eb912e92aaab6ae645d5010faa1564
JessycaTaynara <jessycatay16@gmail.com>
2023-12-25 11:31:44 -0300

    entidades

---
+++ src/modules/gato/dtos/gatoDTO.ts
@@ -0,0 +1,9 @@
+export interface GatoDTO {
+  nome: string;
+  sexo: string;
+  raca: string;
+  cor: string;
+  adotado: boolean;
+  descricao: string;
+  id: number;
+}
---
+++ src/modules/gato/entities/gatoEntity.ts
@@ -0,0 +1,9 @@
+export interface GatoEntity {
+  nome: string;
+  sexo: string;
+  raca: string;
+  cor: string;
+  adotado: boolean;
+  descricao: string;
+  id: number;
+}
