52c55ae759acbd09f0d97884cd43f0182c6f0d61
JessycaTaynara <jessycatay16@gmail.com>
2023-12-25 20:16:44 -0300

    atualizar recebe um gato

--- src/modules/gato/repositories/gato.repository.ts
+++ src/modules/gato/repositories/gato.repository.ts
@@ -5,6 +5,6 @@ export interface GatoRepository {
   deletar(id: number): Promise<void>;
   listar(): Promise<GatoEntity[]>;
   listarUm(id: number): Promise<GatoEntity>;
-  atualizar(id: number, nome:string, sexo:string, raca:string, cor:string, adotado: boolean, descricao:string):Promise<void>
+  atualizar(id: number, gato:GatoEntity):Promise<void>
   adotar(id:number):Promise<void>
 }
