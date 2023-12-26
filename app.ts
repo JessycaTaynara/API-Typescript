71b5890f0e95358f44c501db273028012476eff0
JessycaTaynara <jessycatay16@gmail.com>
2023-12-26 20:22:13 -0300

    agora a api liga direito

--- app.ts
+++ app.ts
@@ -4,7 +4,7 @@ import routes from "./src/routes/routes"
 const app = express()
 const porta = 3000
 
-app.use(express.json)
+app.use(express.json())
 app.use(routes)
 app.listen(porta, ()=>{
     console.log(`API rodando na porta ${porta}`)
