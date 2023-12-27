cb7fa0436d4df75e5b34498ba3f8cca14c73dde4
JessycaTaynara <jessycatay16@gmail.com>
2023-12-26 21:00:17 -0300

    adicionando await

--- src/modules/gato/controllers/listar-gato.controller.ts
+++ src/modules/gato/controllers/listar-gato.controller.ts
@@ -5,7 +5,10 @@ export class ListarGatosController{
     constructor(private readonly service: ListarGatoService){}
     async handle(req: Request, res: Response):Promise<Response>{
         try {
-            const gatos = this.service.execute()
+            const gatos = await this.service.execute()
+            if(gatos.length === 0){
+                return res.status(404).send({message: "Não há gatos cadastrados"})
+            }
             return res.status(200).json(gatos)
         } catch (error) {
             return res.status(500).send({message: "Erro ao listar gatos"})
